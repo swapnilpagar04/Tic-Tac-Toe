9bf4b8ec01c6e43114b9606a8452d14f62f169ae
Swapnil-pagar <swapnilpagar04@gmail.com>
2025-04-16 06:15:52 +0530

    changes in package

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Tic-Tac-Toe/', // 👈 Very important for GitHub Pages
 })
